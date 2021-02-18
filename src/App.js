99bf882303d9944f82b160c1beccc625697be1cc
Anugerah Erlaut <aerlaut@live.com>
2021-02-18 19:44:17 +0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
+import React, { useCallback } from 'react'
+import { useDropzone } from 'react-dropzone';
+import Amplify, { Storage } from 'aws-amplify';
+
 import './App.css';
 
+// Configure Amplify
+Amplify.configure({
+  Auth: {
+    identityPoolId: 'eu-west-1:31a9ed78-b2a6-4c3c-b10b-7faf7b90e0ca', //REQUIRED - Amazon Cognito Identity Pool ID
+    region: 'eu-west-1', // REQUIRED - Amazon Cognito Region
+  },
+  Storage: {
+    AWSS3: {
+      bucket: 'biomage-agi-test-bucket', //REQUIRED -  Amazon S3 bucket
+      region: 'eu-west-1', //OPTIONAL -  Amazon service region
+    }
+  }
+});
+
+// Configure Amplify to not use prefix when uploading to public folder, instead of '/'
+Storage.configure({
+  customPrefix: {
+    public: '',
+  },
+})
+
 function App() {
+
+
+  // Handle on Drop
+  const onDrop = useCallback((acceptedFiles) => {
+    acceptedFiles.forEach((file) => {
+
+      // First character of file.path === '/' means a directory is uploaded
+      // Remove initial slash so that it does not create an empty directory in S3
+      const filePath = file.path[0] === '/' ? file.path.slice(1) : file.path
+
+      // Upload to AWS Amplify
+      Storage.put(filePath, file)
+        .then(result => console.log(result))
+        .catch(err => console.log(err));
+    })
+
+  }, [])
+
+  const { getRootProps, getInputProps } = useDropzone({ onDrop });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <main className="App">
+      <h1>Amplify Drag and Drop Upload</h1>
+      <div {...getRootProps({ className: 'dropzone' })} id="dropzone">
+        <input {...getInputProps()} />
+        <p>Drag and drop some files here, or click to select files</p>
+      </div>
+    </main>
+  )
 }
 
 export default App;
